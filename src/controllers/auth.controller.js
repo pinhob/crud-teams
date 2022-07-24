bc3cd2244cd6fa24635a984e2a03de18ca63a69f
Bruno Pinho <brunopinho@outlook.com>
2022-07-24 18:32:12 -0300

    feat: create register validations in auth service

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,12 +1,12 @@
 const authService = require('../services/auth.service');
 
-const register = async (req, res) => {
+const register = async (req, res, next) => {
   try {
     const user = await authService.register(req.body);
 
     res.status(200).json(user);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
 
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,20 @@
 const User = require('../database/models/User');
+const Joi = require('joi');
+const errorMessage = require('../utils/errorMessageConstructor');
+
+const authSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+})
 
 const register = async (data) => {
+  const { error } = authSchema.validate(data);
+  if (error) throw (errorMessage(400, error.message));
+
+  const userExists = await User.findOne({ email: data.email });
+  if (userExists) throw (errorMessage(400, 'User already exists'));
+
   const user = await User.create(data);
 
   return user;
